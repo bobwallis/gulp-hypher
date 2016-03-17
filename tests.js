9a9f18219b2265532aeae8835443112491628d43
Bob Wallis <bob.wallis@gmail.com>
2016-03-17 23:35:54 +0000

    Extend tests to cover entity numbers

--- tests.js
+++ tests.js
@@ -12,11 +12,11 @@ describe('gulp-hypher', function() {
 	it('should ', function (cb) {
 		var stream = gulpHypher(hyphenation_pattern);
 		var html = '<p>Council leaders have called on the government to provide more resources to help them house extra refugees that the UK is planning to accept.</p>'
-			+ '<p>... and then I just test if it also works with HTML entities</p>'
-			+ '<p>dagger -> &dagger; <br>kappa -> &kappa; <br>omicron -> &omicron; </p>';
+			+ '<p>Also test HTML entities</p>'
+			+ '<p>dagger -> &dagger; <br>kappa -> &kappa; <br>omicron -> &omicron; <br> lt -> &#60; <br> ã -> a&#771;</p>';
 		var result = '<p>Coun\u00ADcil lead\u00ADers have called on the gov\u00ADern\u00ADment to provide more re\u00ADsources to help them house ex\u00ADtra refugees that the UK is plan\u00ADning to ac\u00ADcept.</p>'
-			+ '<p>... and then I just test if it also works with HTML en\u00ADtit\u00ADies</p>'
-			+ '<p>dag\u00ADger -> &dagger; <br>kappa -> &kappa; <br>omic\u00ADron -> &omicron; </p>';
+			+ '<p>Also test HTML en\u00ADtit\u00ADies</p>'
+			+ '<p>dag\u00ADger -> &dagger; <br>kappa -> &kappa; <br>omic\u00ADron -> &omicron; <br> lt -> &#60; <br> ã -> a&#771;</p>';
 		var file = new gutil.File({
 			base: __dirname,
 			path: __dirname + '/test.html',
